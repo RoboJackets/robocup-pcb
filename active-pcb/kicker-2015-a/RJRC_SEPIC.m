cfc9eddd50456eccb9abaab5273c6b37f4e749f2
gtrosawa <rosawa@gatech.edu>
2015-04-20 11:55:53 -0400

    Selected operating frequency and inductor

--- active-pcb/kicker-2015-a/RJRC_SEPIC.m
+++ active-pcb/kicker-2015-a/RJRC_SEPIC.m
@@ -4,17 +4,17 @@
 % use of a SEPIC 
 
 %% Target Parameters
-Vin  = 18.5; % Input voltage [V]
-Vout = 250;  % Output voltage [V]
-Vd   = 0.1;  % Diode forward voltage [V]
-
-f = [100e3 1000e3]; % Operating frequency [Hz]
+Vin      = 18.5;  % Input voltage [V]
+Vout     = 250;   % Output voltage [V]
+Vd       = 0.1;   % Diode forward voltage [V]
+Iout_max = 10;    % Maximum output current [A]
+f        = 600e3; % Operating frequency [Hz]
 
 %% Duty Cycle
 D = (Vout + Vd) / (Vin + Vout + Vd); % Maximum duty cycle
 
 %% Inductor Selection
-Isw_max = I0_max / (1-D); % Maximum switch current
+Isw_max = Iout_max / (1-D); % Maximum switch current
 chi = 0.2; % Percentage peak-to-peak ripple current
 Isw_pk  = (1 + chi/2) * Isw_max; % Peak switch current
 dIsw = chi * Isw_max; % Switch ripple current
@@ -26,4 +26,4 @@ Rsense = 80e-3 / Isw_pk; % Sense resistor value [ohm]
 
 %% Result
 L
-Rsense
+Rsense;
