382e0d11d6abc146cd3825efccf487f44d18ee0d
gtrosawa <rosawa@gatech.edu>
2015-05-27 08:18:03 -0400

    SEPIC sim initial commit

--- active-pcb/kicker-2015-a/RJRC_SEPIC.m
+++ active-pcb/kicker-2015-a/RJRC_SEPIC.m
@@ -1,29 +1,48 @@
 %% Overview
 % This simulation will determine the component parameters required for the
 % RoboJackets RoboCup SSL kicker board. This simulation will consider the
-% use of a SEPIC 
+% use of the SEPIC configuration using Linear Technology's LT3757A IC.
 
 %% Target Parameters
-Vin      = 18.5;  % Input voltage [V]
-Vout     = 250;   % Output voltage [V]
-Vd       = 0.1;   % Diode forward voltage [V]
-Iout_max = 10;    % Maximum output current [A]
-f        = 600e3; % Operating frequency [Hz]
-
+Vin      = 18.5;    % Input voltage [V]
+Vout     = 240;     % Output voltage [V]
+Vd       = 0;       % Diode forward voltage [V]
+IL1_max  = 21;      % Inductor saturation current [A]
+f        = 700e3;  % Operating frequency [Hz]
+Tss      = 12.5e-3; % Soft-start interval [s]
+Tss=.5e-3;
 %% Duty Cycle
 D = (Vout + Vd) / (Vin + Vout + Vd); % Maximum duty cycle
 
 %% Inductor Selection
+Iout_max = IL1_max * (1-D) / D; % Maximum output current [A]
 Isw_max = Iout_max / (1-D); % Maximum switch current
 chi = 0.2; % Percentage peak-to-peak ripple current
 Isw_pk  = (1 + chi/2) * Isw_max; % Peak switch current
 dIsw = chi * Isw_max; % Switch ripple current
 
+L1 = Vin * D ./ (0.5 * dIsw * f);
 L = (Vin * D) ./ (dIsw * f); % Inductance value [H]
 
 %% Sense Resistor Selection
 Rsense = 80e-3 / Isw_pk; % Sense resistor value [ohm]
 
+%% FET Selection
+% G_loss   = Isw_max^2 * Rds_on * D; % Conduction loss [W]
+% SW_loss  = 2 * (Vin + Vout)^2 * I * Crss * f;
+% P_rating = G_loss + SW_loss; % Required FET Power rating [W]
+
+%% Diode Selection
+Id_pk = (1 + chi/2) *  Iout_max * 1/(1-D);
+
+%% Capacitor Selection
+% The specific value of Cdc is not significant
+Irms_Cdc = Iout_max * sqrt((Vout+Vd) / Vin); % Minimum RMS current rating 
+V_Cdc = Vin; % Minimum voltage rating
+Cout = Iout_max ./ (0.01 * Vout * f); % Minimum load capacitance
+
+%% Soft-Start Capacitor
+Css = Tss * 10e-6/1.25; % Capacitance given the soft-start interval
+
 %% Result
-L
-Rsense;
+RJRC_SEPIC_showResult;
---
+++ active-pcb/kicker-2015-a/RJRC_SEPIC_showResult.m
@@ -0,0 +1,9 @@
+%Result=table(D,L1,Rsense)
+%Rt_list = [140 63.4 41.2 30.9 24.3 19.6 16.5 14 12.1 10.5] * 1e3;
+%table(f'./1e3,L1'.*1e6,L'.*1e6,Rt_list')
+format shorteng
+f
+L
+Rsense
+Cout
+L1
