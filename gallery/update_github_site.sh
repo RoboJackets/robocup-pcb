cefde73e5b551b82985b1737f523d72ac4df3165
Justin Buchanan <justbuchanan@gmail.com>
2016-05-18 14:01:40 -0400

    added script to update gh-pages

---
+++ gallery/update_github_site.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+REPO_DIR="./" # TODO: fix - make it point to repo root
+
+TMP_DIR=$(mktemp -d)
+GALLERY_DIR=$TMP_DIR/robocup-pcb
+
+# clone repo to tmp dir and checkout gh-pages branch, then remove old content
+git clone $REPO_DIR $GALLERY_DIR
+pushd $GALLERY_DIR
+git checkout gh-pages
+rm -r ./*
+popd
+
+# run gallery-generation python script and place output in tmp repo dir
+$REPO_DIR/gallery/main.py $REPO_DIR -o $GALLERY_DIR
+
+# set git info
+git config --global user.name "$GIT_USERNAME"
+git config --global user.email "$GIT_EMAIL"
+
+# commit and push
+pushd $GALLERY_DIR
+git add .
+git commit -m 'updated pcb gallery'
+git push git@github.com/robojackets/robocup-pcb gh-pages
+popd
